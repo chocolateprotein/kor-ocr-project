cf121dec6686dd7c7a9c5c7dad11899274ed5146
chocolateprotein <108640819+chocolateprotein@users.noreply.github.com>
2022-12-08 20:58:44 +0900

    my project

---
+++ easyocr/ocr.sh
@@ -0,0 +1,7 @@
+
+easyocr -l ko en -f ~/desktop/과학2_미래엔_page-0003.jpg --detail=1 --gpu=False
+#ocr 파일 안에 jpg를 넣고 실행
+#실행문은 sh ocr.sh
+
+#6 - 0.7, 7 - 0.7 , 8 - 0.5 이하 , 9 - 0.5 이하 
+#타임 30~1분
